88ca282b1f66c241ccb1d922134b53eefe861af9
4a50 <jpjones22@gmail.com>
2021-03-26 14:12:22 -0700

    Renamed and Restructured View Inventory

---
+++ Gamesis5000/Views/HomePage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using Gamesis5000.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gamesis5000.Views
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class HomePage : ContentPage
+  {
+    readonly HomePageViewModel _vm;
+    public ICommand NavigateCommand { get; private set;  }
+    Page page;
+    public HomePage()
+    {
+      InitializeComponent();
+      NavigateCommand = new Command<Type>(async (Type pageType) =>
+      {
+        page = (Page)Activator.CreateInstance(pageType);
+        await Navigation.PushAsync(page);
+      });
+      BindingContext = this;
+
+      Title = "Gamesis 5000X";
+    }
+
+    private async Task OnClick(object sender, EventArgs e)
+    {
+      Button clickedButton = (Button)sender;
+      Debug.WriteLine($"[Dev Note] OnClick() Fired: {clickedButton.StyleId}");
+
+      switch (clickedButton.StyleId)
+      {
+        case "ViewInventoryButton":
+          
+          break;
+      }
+    }
+  }
+}
---
+++ Gamesis5000/Views/ViewInventoyPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Gamesis5000.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gamesis5000.Views
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class ViewInventoyPage : ContentPage
+  {
+    readonly ViewInventoryViewModel _vm;
+    public ViewInventoyPage()
+    {
+      InitializeComponent();
+      BindingContext = _vm = new ViewInventoryViewModel();
+      Title = "View Inventory";
+    }
+  }
+}
