4dd149faca8cd62f3634eae15558f217ebf3806d
4a50 <jpjones22@gmail.com>
2021-03-29 17:00:41 -0700

    Homepage passes search criteria to SearchResults page.

--- Gamesis5000/Views/HomePage.xaml.cs
+++ Gamesis5000/Views/HomePage.xaml.cs
@@ -1,4 +1,5 @@
-﻿using Gamesis5000.ViewModels;
+﻿using Gamesis5000.Models;
+using Gamesis5000.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
@@ -16,11 +17,15 @@ namespace Gamesis5000.Views
   {
   public string Text { get; private set; }
 
+    public SearchParameters searchParams;
+
+
     readonly HomePageViewModel _vm;
     public ICommand NavigateCommand { get; private set;  }    
     public HomePage()
     {
       InitializeComponent();
+      searchParams = new SearchParameters {SearchByFilter = "Title", SearchByDatabase = true };
       //Create an instance of a page. Then navigates to it.
       NavigateCommand = new Command<Type>(async (Type pageType) =>
       {
@@ -40,11 +45,45 @@ namespace Gamesis5000.Views
 
     
 
-    private void OnSearchButtonPressed(object sender, EventArgs e)
+    async void OnSearchButtonPressed(object sender, EventArgs e)
     {
       SearchBar search = (SearchBar)sender;
       Text = search.Text;
-      Debug.WriteLine($"[Dev Note] Search Button Pressed Text: {search.Text}");
+      searchParams.SearchString = search.Text;
+      Debug.WriteLine($"[Dev Note] Search Button Pressed Sending: string: {searchParams.SearchString} filter: {searchParams.SearchByFilter} searchbyDbase: {searchParams.SearchByDatabase}");
+      await Navigation.PushAsync(new SearchResultsPage(searchParams), true);
+    }
+
+    private void OnCheckChanged(object sender, CheckedChangedEventArgs e)
+    {
+      RadioButton rb = (RadioButton)sender;
+      string value = (string)rb.Value;
+      if (value == null) Debug.WriteLine("[Dev Critical] value is null");
+      Debug.WriteLine($"[Dev Note] rbVal to String: ");
+      if (e.Value && value != null)
+      {
+        if (rb.GroupName == "SearchFilter")
+        {
+          searchParams.SearchByFilter = value;
+        }
+        else if (rb.GroupName == "SourceSearch")
+        {
+          Debug.WriteLine($"[Dev Note] Source Search Updated: ");
+          bool.TryParse(value, out bool byDatabase);
+          searchParams.SearchByDatabase = byDatabase;
+        }
+        else
+        {
+          Debug.WriteLine($"[Dev Note] Nothing Matches Radio Button Values");
+        }
+
+        Debug.WriteLine($"[Dev Note] RadioValue: {rb.Value}");
+        Debug.WriteLine($"[Dev Note] GroupName: {rb.GroupName}");
+      }
+
+      
+
+      //Debug.WriteLine($"[Dev Note] Radio Button {rb.Value}Checked  Args: {e.Value}");
     }
   }
 }
