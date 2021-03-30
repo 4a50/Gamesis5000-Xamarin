541ffde1fcabc0206aab7485a8811b8d4329b776
4a50 <jpjones22@gmail.com>
2021-03-30 10:33:40 -0700

    Successfully carried data from HomePage

---
+++ Gamesis5000/Services/APIService.cs
@@ -0,0 +1,25 @@
+﻿using Gamesis5000.Models;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gamesis5000.Services
+{
+  public class APIService
+  {
+    HttpClient client;
+    string BaseUrl = "https://api.thegamesdb.net/v1/Games/ByGameName?apikey=4c9d180c15bf5fe3e896c204472a85c752dddb4fcdf0ba291b00b037af1c1910&name=super%20Metroid&filter%5Bplatform%5D=6";
+    public APIService()
+    {
+      client = new HttpClient();
+    }
+
+    public async Task<Game> GetDataAsync()
+    {
+      Uri uri = new Uri(;
+      return new Game();
+    }
+  }
+}
--- Gamesis5000/ViewModels/SearchResultsViewModel.cs
+++ Gamesis5000/ViewModels/SearchResultsViewModel.cs
@@ -2,21 +2,27 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using MvvmHelpers;
+using System.ComponentModel;
+using System.Diagnostics;
 
 namespace Gamesis5000.ViewModels
 {
-  class SearchResultsViewModel : BaseViewModel
+  public class SearchResultsViewModel : BaseViewModel, INotifyPropertyChanged
   {
     public IObservable<SearchParameters> vmParams;
     SearchParameters searchParams;
-    SearchResultsViewModel()
+    string searchString = "";
+
+    public string SearchStringDisplay { get { return searchString; } }
+    public SearchResultsViewModel()
     {
       searchParams = new SearchParameters();
     }
-    SearchResultsViewModel(SearchParameters inParams)
+    public SearchResultsViewModel(SearchParameters inParams)
     {
       searchParams = inParams;
-      
+      searchString = searchParams.SearchString;
     }
   }
 }
