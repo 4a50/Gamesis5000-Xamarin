a8f0a5714e6dd336421b2d93d281a9f366cb2cd0
4a50 <jpjones22@gmail.com>
2021-04-08 17:55:21 -0700

    Started Initial wiring for the Game Detail page

---
+++ Gamesis5000/ViewModels/GameDetailViewModel.cs
@@ -0,0 +1,21 @@
+﻿using Gamesis5000.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Gamesis5000.ViewModels
+{
+  public class GameDetailViewModel : BaseViewModel
+  {
+    Game game { get; set; }
+    public GameDetailViewModel()
+    {
+      game = new Game();
+    }
+    public GameDetailViewModel(Game paramGame)
+    {
+      game = paramGame;      
+    }
+
+  }
+}
---
+++ Gamesis5000/Views/GameDetailView.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Gamesis5000.Views
+{
+  [XamlCompilation(XamlCompilationOptions.Compile)]
+  public partial class GameDetailView : ContentPage
+  {
+    public readonly GameDetailView _vm;
+    public GameDetailView()
+    {
+      InitializeComponent();
+      BindingContext = _vm = new GameDetailView();
+      //Create an instance of a page. Then navigates to it.
+      Title = "View Inventory";
+    }
+  }
+}
